9c9a0ff7ca4fd59e8574862e6bbb10cab9985ade
aguaholic <joyterra@gmail.com>
2020-04-14 15:48:57 +0200

    Add styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,19 @@ const TitleAndDescription = ({data}) => {
     const title = data.site.siteMetadata.title
     const description = data.site.siteMetadata.description
     return(
-        <div>
-            <h2>{title}</h2>
-            <p>{description}</p>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            fontFamily: 'avenir'
+        }}>
+            <h2 style={{ marginBottom: 0 }}>{title}</h2>
+            <p  style={{
+                marginTop: 0,
+                opacity: 0.5            
+            }}>
+                {description}
+            </p>
         </div>
     )
 }
