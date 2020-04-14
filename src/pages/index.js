22fc1bd84149e231a8bb4e28e027d20029d5ed9d
aguaholic <joyterra@gmail.com>
2020-04-14 18:10:10 +0200

    Add graphql query for all posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,42 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Header from  '../components/Header'
 
-const Layout = () => {
+const Layout = ({ data }) => {
+    const { edges } = data.allMarkdownRemark
     return (
         <div>
             <Header />
+            {edges.map(edge => {
+                const {frontmatter} = edge.node
+                return(
+                    <div key={frontmatter.path}>
+                        {frontmatter.title}
+                    </div>
+                )
+            })}
         </div>
     )
 }
+
+export const query = graphql`
+    query HomepageQuery {
+        allMarkdownRemark(
+            sort: {order: DESC, fields: frontmatter___date}
+        ) {
+            edges {
+                node {
+                    frontmatter {
+                        title
+                        path
+                        date
+                        excerpt
+                    }
+                }
+            }
+        }
+    }
+`
 
 export default Layout
